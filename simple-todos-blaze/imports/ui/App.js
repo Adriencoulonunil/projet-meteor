1f403c4e18317b10a29dfd1eff8d3d5a04dc55c3
Adriencoulonunil <adrien.coulon@unil.ch>
2022-03-23 18:30:03 +0100

    6.1

--- simple-todos-blaze/imports/ui/App.js
+++ simple-todos-blaze/imports/ui/App.js
@@ -1,13 +1,28 @@
 import { Template } from 'meteor/templating';
 import { TasksCollection } from "../api/TasksCollection";
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './App.html';
 import './Task.js';
+
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.mainContainer.helpers({
   tasks() {
     return TasksCollection.find({}, { sort: { createdAt: -1 } });
   },
 });
+
+Template.mainContainer.events({
+  "click #hide-completed-button"(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  }
+});
+
 Template.form.events({
   "submit .task-form"(event) {
     // Prevent default browser form submit
